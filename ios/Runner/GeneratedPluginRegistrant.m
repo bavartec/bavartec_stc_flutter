84caee9d72c981f2181f9ac80326109a65f45aa5
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-11-15 04:09:11 +0100

    Upgrade dependencies

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -5,7 +5,6 @@
 #import "GeneratedPluginRegistrant.h"
 #import <connectivity/ConnectivityPlugin.h>
 #import <device_info/DeviceInfoPlugin.h>
-#import <permission_handler/PermissionHandlerPlugin.h>
 #import <shared_preferences/SharedPreferencesPlugin.h>
 #import <smartconfig/SmartconfigPlugin.h>
 
@@ -14,7 +13,6 @@
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
   [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
-  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
   [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
   [SmartconfigPlugin registerWithRegistrar:[registry registrarForPlugin:@"SmartconfigPlugin"]];
 }
